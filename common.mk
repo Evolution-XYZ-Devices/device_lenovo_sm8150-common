8b3c185aa9a16eeea637ae292fc08946d743e47e
althafvly <althafvly@gmail.com>
2023-09-04 22:27:18 +0000

    sm8150-common: Build fastcharge implementation

--- common.mk
+++ common.mk
@@ -234,6 +234,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm-service.clearkey
 
+# Fastcharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.lenovo_sm8150
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.lenovo_msmnile
