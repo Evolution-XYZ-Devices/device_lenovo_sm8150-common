0297c898e6c6cac42c3b542787746086eb4fdb5c
kenway214 <kenway214@outlook.com>
2023-10-04 21:13:12 +0200

    sm8150-common: Add Basic Call Recording support

--- common.mk
+++ common.mk
@@ -132,6 +132,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
 
+# Basic Call Recording
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbthost_if \
